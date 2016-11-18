6abada01a9a8367ed6a6e0dc1c70b8479ec6f57a
Eric <eric.belter@live.com>
2016-11-18 15:11:56 -0600

    - Test Page now loads but had to comment out one section of it to make load

--- teamb2l/app/handlers/TestHandler.py
+++ teamb2l/app/handlers/TestHandler.py
@@ -1,20 +1,19 @@
 import webapp2
 import jinja2
 import os
-from ..tests.Test_StudentAcct import StudentTest
+from ..tests import Test_StudentAcct
 
 import main
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), '..', 'Mocs')),
 extensions=['jinja2.ext.autoescape'],
 autoescape=True)
 
-
 class TestHandler(webapp2.RequestHandler):
     def get(self):
-        x = StudentTest()
-        template = JINJA_ENVIRONMENT.get_template('TestPage.html')
+        x = Test_StudentAcct.StudentTest
+        template = JINJA_ENVIRONMENT.get_template('/TestPage.html')
         self.response.write(template.render({
-            't1': x.test_makeStudent()
+            #'t1': x.test_makeStudent()
         }))
