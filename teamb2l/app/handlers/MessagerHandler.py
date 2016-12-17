06a5d9646a4f147c4825091e531b9b6b1a01d763
Jacob Kilgore <jkilgore@uwm.edu>
2016-12-17 17:46:00 -0600

    Working on Messager class

--- teamb2l/app/handlers/MessagerHandler.py
+++ teamb2l/app/handlers/MessagerHandler.py
@@ -4,33 +4,17 @@ import os
 from teamb2l.app.classes import User
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), '..', 'web', 'views', 'user')),
+loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), '..', 'web', 'views')),
 extensions=['jinja2.ext.autoescape'],
 autoescape=True)
 
 
 class MessageHandler(webapp2.RequestHandler):
 
-    def refresh(message_log):
-        for message in message_log:
-            sender = message[0]
-            text = message[1]
-            html_class = ""
-            html = ""
-            if User.name == sender:
-                html_class = "selfMessage"
-            else:
-                html_class = "otherMessage"
-            html.append("<div class=" + html_class + ">" + text + "</div>")
-        return html
-
-    def refresh(self):
-        message_log = []
-        self.refresh(message_log)
-
-    JINJA_ENVIRONMENT.filters['refresh'] = refresh
-
     def get(self):
+        user = User.User.email
+        messages = self.request.query()
+
         template = JINJA_ENVIRONMENT.get_template('/messager.html')
         self.response.write(template.render())
 
