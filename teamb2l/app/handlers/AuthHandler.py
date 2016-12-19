23c5167e5bee6a3363b22fb2f66c92a31ab72f28
Jacob Kilgore <jkilgore@uwm.edu>
2016-12-19 17:21:16 -0600

    Revert "Condensed html and css"
    This reverts commit f21ac0461f573e08dc8c3032845b3672fea9ffe9.

--- teamb2l/app/handlers/AuthHandler.py
+++ teamb2l/app/handlers/AuthHandler.py
@@ -12,12 +12,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 class RegisterHandler(Handler):
-    def render_page(self, name="", email="", password="", confirm_pass="", error=""):
-        self.render('/auth/register.html', name=name, email=email, password=password, confirm_pass=confirm_pass,
-                    error=error)
-
     def get(self):
-        self.render_page()
+        self.render('/auth/register.html')
 
     def post(self):
         name = self.request.get('name')
@@ -25,35 +21,28 @@ class RegisterHandler(Handler):
         password = self.request.get('password')
         confirm_pass = self.request.get('confirm')
 
-        if name == "" or email == "" or password == "" or confirm_pass == "":
-            self.render_page(name=name, email=email, password=password, confirm_pass=confirm_pass,
-                             error="Fill in missing fields")
+        user = User.query(User.email == email).get()
 
+        logging.info(user)
+
+        if user != None:
+            self.render('/auth/register.html', errors="Error: Email already taken")
+        elif password != confirm_pass:
+            self.render('/auth/register.html', errors="Passwords do not match")
         else:
-            user = User.query(User.email == email).get()
+            user = User()
 
-            logging.info(user)
-
-            if user != None:
-                self.render_page(name=name, email=email, password=password, confirm_pass=confirm_pass,
-                                 error="Email already taken")
-            elif password != confirm_pass:
-                self.render_page(name=name, email=email, password=password, confirm_pass=confirm_pass,
-                                 error="Passwords do not match")
-            else:
-                user = User()
-
-                user.email = email
-                user.password = password
-                user.name = name
-                user_key = user.put()
-                self.response.set_cookie('user', user_key.urlsafe())
-                postMe = """<body style="background-color:rgb(45, 45, 45);">
-                            <center><img src="/assets/giphy.gif" alt="Check"><br>
-                            <font color="Gold" style="font-family:Montserrat;">
-                            Registration Successful </font></center></body>
-                            <meta http-equiv="refresh" content="2;url=/">"""
-                self.response.write(postMe)
+            user.email = email
+            user.password = password
+            user.name = name
+            user_key = user.put()
+            self.response.set_cookie('user', user_key.urlsafe())
+            postMe = """<body style="background-color:rgb(45, 45, 45);">
+                        <center><img src="/assets/giphy.gif" alt="Check"><br>
+                        <font color="Gold" style="font-family:Montserrat;">
+                        Registration Successful </font></center></body>
+                        <meta http-equiv="refresh" content="2;url=/">"""
+            self.response.write(postMe)
 
 class LoginHandler(Handler):
     def get(self):
